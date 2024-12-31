a29b88f3e6067afef24d712bb34125e8e1e8642a
benjameep <14208829+benjameep@users.noreply.github.com>
2024-12-31 16:54:48 -0700

    auto commit

--- cron.sh
+++ cron.sh
@@ -1,1 +1,2 @@
-/Users/anaconda3/bin/python build && git add . && git commit -m "auto commit" && git push
+#!/bin/bash
+cd ~/Documents/opus-magnum-triangle && /Users/anaconda3/bin/python build && git add . && git commit -m "auto commit" && git push
